2a2531fe91eef539a2371d54cd42c0aa466723cf
ririssilvia <ririssilvia1@gmail.com>
2021-02-26 12:48:02 +0700

    first commit-Statefulwidget Week3

---
+++ lib/Convert.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Convert extends StatelessWidget {
+ final Function konvertHandler;
+Convert({Key key, @required this.konvertHandler});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 420,
+      height: 50,
+      child: (
+        // ignore: deprecated_member_use
+        RaisedButton(
+          onPressed: konvertHandler,
+          color: Colors.blue,
+          textColor: Colors.white,
+          child: const Text('Konversi Suhu'),
+        )
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Input.dart';
+import 'Result.dart';
+import 'Convert.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatefulWidget {
+  
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //controller
+  TextEditingController etInput = new TextEditingController();
+  final  konvertHandler = Function;
+
+  //variabel berubah
+  double _inputUser = 0;
+  double _kelvin = 0;
+  double _reamur = 0;
+  
+  //fungsi
+  void _konversiSuhu(){
+    setState(() {
+      _inputUser = double.parse(etInput.text);
+      _kelvin = _inputUser + 273;
+      _reamur = _inputUser * (4/5);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Konverter Suhu"),
+          ),
+          body: Container(
+            margin: EdgeInsets.all(8),
+            child: Column(
+              children: [
+                Input(etInput: etInput),
+                //memperluas anak row
+                Result(kelvin: _kelvin, reamur: _reamur),
+                Convert(konvertHandler: _konversiSuhu), 
+              ],
+            ),
+          ),
+        )
+      );
+  }
+}
+
+
+
+
